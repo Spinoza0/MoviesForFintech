2467255c10bd0536847061c613453b795b993d14
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-02-03 17:38:15 +0300

    Initial commit

---
+++ app/src/main/java/com/spinoza/moviesforfintech/PopularFilmsActivity.kt
@@ -0,0 +1,11 @@
+package com.spinoza.moviesforfintech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopularFilmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_films)
+    }
+}
