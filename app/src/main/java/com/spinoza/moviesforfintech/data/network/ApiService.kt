c94d29ac4735d2143a2624fb4521bedd51ba718d
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-02-04 16:17:59 +0300

    fix using API-KEY and add cache-control

--- app/src/main/java/com/spinoza/moviesforfintech/data/network/ApiService.kt
+++ app/src/main/java/com/spinoza/moviesforfintech/data/network/ApiService.kt
@@ -2,29 +2,22 @@ package com.spinoza.moviesforfintech.data.network
 
 import com.spinoza.moviesforfintech.data.network.model.FilmDescriptionDto
 import com.spinoza.moviesforfintech.data.network.model.ResponseDto
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ApiService {
 
+    @Headers("X-API-KEY: $API_KEY", "Cache-Control: max-age=$CACHE_MAX_AGE_SECONDS")
     @GET("top?type=TOP_100_POPULAR_FILMS")
-    suspend fun getTopPopularFilms(
-        @Header("X-API-KEY: $API_KEY")
-        @Query(QUERY_PARAM_PAGE) page: Int,
-    ): ResponseDto
+    suspend fun getTopPopularFilms(@Query(QUERY_PARAM_PAGE) page: Int): ResponseDto
 
-
+    @Headers("X-API-KEY: $API_KEY", "Cache-Control: max-age=$CACHE_MAX_AGE_SECONDS")
     @GET("top/{$QUERY_PATH_ID}")
-    suspend fun getFilmDescription(
-        @Header("X-API-KEY: $API_KEY")
-        @Path(QUERY_PATH_ID) id: Int,
-    ): FilmDescriptionDto
+    suspend fun getFilmDescription(@Path(QUERY_PATH_ID) id: Int): FilmDescriptionDto
 
     companion object {
         private const val QUERY_PARAM_PAGE = "page"
         private const val QUERY_PATH_ID = "id"
+        private const val CACHE_MAX_AGE_SECONDS = 36000
 
         private const val API_KEY = "d985726c-2563-4b5a-bed8-0857d1b8944c"
         // key from tinkoff
